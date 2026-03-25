b922ea0ab494d9ca218f572626031ea83573887d
oJonasRtz <jonasper19@gmail.com>
2026-03-25 19:09:29 +0000

    refactor backend

---
+++ backend/src/objects/Server.py
@@ -0,0 +1,24 @@
+from flask import Flask, jsonify, request
+from src.objects.Routes import Routes
+from src.objects.utils.set_middlewares import set_middlewares
+
+class Server:
+    def __init__(self):
+        self.app = Flask(__name__)
+        self.routes = Routes()
+        
+        set_middlewares(self.app)
+        self._set_upload_limit(10)
+        self.routes.load_routes(self.app)
+        self._set_error_handlers()
+
+    def _set_upload_limit(self, limit_mb):
+        self.app.config['MAX_CONTENT_LENGTH'] = limit_mb * 1024 * 1024
+    
+    def _set_error_handlers(self):
+        @self.app.errorhandler(413)
+        def handle_file_too_large(e):
+            return jsonify({"error": "File is too large. Maximum size is 10 MB"}), 413
+    
+    def run(self, host="", port=5000, ssl_context=None):
+        self.app.run(host=host, port=port, ssl_context=ssl_context)
