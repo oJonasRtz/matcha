397dc2796344dc2ace860f8ea1c7288c4eaa81a3
oJonasRtz <jonasper19@gmail.com>
2026-05-26 19:46:52 +0000

    default admin

--- database/init/14_moderators.sql
+++ database/init/14_moderators.sql
@@ -1,4 +1,5 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+CREATE EXTENSION IF NOT EXISTS pgcrypto;
 
 CREATE TABLE IF NOT EXISTS moderators (
 	id SERIAL PRIMARY KEY,
@@ -7,3 +8,7 @@ CREATE TABLE IF NOT EXISTS moderators (
 	email VARCHAR(255) UNIQUE NOT NULL,
 	password_hash VARCHAR(255) NOT NULL
 );
+
+INSERT INTO moderators (username, email, password_hash)
+VALUES ('admin', 'admin@mod.com', crypt('admin', gen_salt('bf')))
+ON CONFLICT (username) DO NOTHING;
